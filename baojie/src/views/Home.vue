4e39b44bc3de91f84d3aae37d998c9d8d49b8b94
dingxiliang <dingxiliang@enn.cn>
2019-09-18 11:18:11 +0800

    some change

--- baojie/src/views/Home.vue
+++ baojie/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="5">
+        <el-input
+          v-model="val"
+          type="number"
+          @keydown.native="down"
+        ></el-input>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -11,8 +18,27 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  data() {
+    return {
+      val: ''
+    }
+  },
+  methods: {
+    down (e) {
+      e.target.value = e.target.value.replace(/[^\d]/g,"")
+    }
+  },
   components: {
     HelloWorld
   }
 }
 </script>
+<style lang="scss">
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none !important;
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+}
+</style>
