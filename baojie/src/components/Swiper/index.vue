04ca5717a3d0c2dce3349f342d876a68f142df2d
dingxiliang <dingxiliang@enn.cn>
2019-10-10 17:28:03 +0800

    添加swipe

---
+++ baojie/src/components/Swiper/index.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+  .swipe swipe组件
+</template>
+
+<script>
+import foo from './foo'
+export default {
+  name: 'Vslot',
+  components: {
+    foo
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- baojie/src/main.js
+++ baojie/src/main.js
@@ -6,12 +6,15 @@ import ElementUI from 'element-ui'
 import VeRing from 'v-charts/lib/ring.common'
 import { ValidationProvider, extend } from 'vee-validate'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 extend('secret', {
   validate: value => value === 'example',
   message: 'msg'
 })
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.use(ElementUI)
+Vue.use(VueAwesomeSwiper)
 Vue.component(VeRing.name, VeRing) // VeRing.name的值就是 VeRing
 Vue.config.productionTip = false
 
