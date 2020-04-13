4f111d516e32d2d2c0d6723439863fa936e38862
dingxl <dingxiliang@knowlegene.com>
2020-04-13 17:52:50 +0800

    add pug

---
+++ rommel/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .login-in-wrapper
+    el-button(type="primary") 登陆
+    el-row
+      el-color-picker(@change="colorChange" v-model="themeColor")
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      themeColor: '#409EFF'
+    }
+  },
+  methods: {
+    colorChange (v) {
+      console.log(v)
+    }
+  }
+}
+</script>
